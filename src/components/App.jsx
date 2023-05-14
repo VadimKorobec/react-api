cba235e52819d4622dfe72d73c1a60d7e22ed7ea
VadimKorobec <_s_e_t__@ukr.net>
2023-05-14 17:51:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import axios from 'axios';
+import { ArticleList } from './ArticleList';
+import api from '../services/api';
+
+axios.defaults.baseURL = 'https://hn.algolia.com/api/v1';
+
+export class App extends Component() {
+  state = {
+    articles: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+    try {
+      const articles = api.fetchArticlesWithQuery('react');
+      this.setState({
+        articles,
+      });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const { articles, isLoading, error } = this.state;
+    return (
+      <div>
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+        {isLoading && <p>Loading...</p>}
+        {articles.length > 0 && <ArticleList articles={articles} />}
+      </div>
+    );
+  }
+}
