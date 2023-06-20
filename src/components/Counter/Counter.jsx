b204c5ebf5809f4428793bd01abb831046ab19c5
VadimKorobec <_s_e_t__@ukr.net>
2023-06-20 16:29:30 +0300

    Create Counter.jsx

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState } from 'react';
+
+export const Counter = () => {
+  const [total, setTotal] = useState(0);
+
+  const handleClick = () => {};
+
+  return <div></div>;
+};
