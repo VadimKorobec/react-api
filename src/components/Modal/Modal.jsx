84d72a85d5c489c96480c417157bcbd918863ab5
VadimKorobec <_s_e_t__@ukr.net>
2023-06-20 16:04:20 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export const Modal = () => {
-  return <div></div>;
+export const Modal = ({ closeModal, children }) => {
+  useEffect(() => {
+    const handlePressESC = e => {
+      console.log('object', Date.now());
+      if (e.code === 'Escape') closeModal();
+    };
+    window.addEventListener('keydown', handlePressESC);
+    return () => {
+      window.removeEventListener('keydown', handlePressESC);
+    };
+  }, [closeModal]);
+
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <h5>Modal</h5>
+            <button type="button" onClick={closeModal}></button>
+          </div>
+          <div>{children}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
