ec393cabd0541a21038586418c9930e44fff5739
VadimKorobec <_s_e_t__@ukr.net>
2023-06-20 16:29:19 +0300

    Create FormaLogin.jsx

---
+++ src/components/FormaLogin/FormaLogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const FormaLogin = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="">
+          Email address
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+};
