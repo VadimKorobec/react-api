20e0cabdf08505fcd7464a3c23e0c1922900a1db
VadimKorobec <_s_e_t__@ukr.net>
2023-06-21 15:04:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,5 @@
-import React, { useState } from 'react';
-import { nanoid } from 'nanoid';
-import { Modal } from './Modal/Modal';
-import { FormaLogin } from './FormaLogin/FormaLogin';
-import { Counter } from './Counter/Counter';
+import React from 'react';
 
 export const App = () => {
-  const [isShowModal, setIsShowModal] = useState(false);
-  const [searchText, setSearchText] = useState('');
-
-  const showModal = () => {
-    setIsShowModal(true);
-  };
-
-  const closeModal = () => {
-    isShowModal(false);
-  };
-
-  const createUser = data => {
-    const newUser = {
-      ...data,
-      id: nanoid(),
-    };
-    console.lof('newUser', newUser);
-  };
-
-  // const handleSearch = searchText => {
-  //   setSearchText(searchText);
-  // };
-
-  return (
-    <div>
-      {/* <Header showModal={showModal} /> */}
-      {/* <Search handleSearch={handleSearch} /> */}
-      {/* <ContentInfo searchText={searchText} /> */}
-      {isShowModal && (
-        <Modal closeModal={closeModal}>
-          <FormaLogin closeModal={closeModal} createUser={createUser} />
-        </Modal>
-      )}
-      <Counter />
-    </div>
-  );
+  return <div></div>;
 };
