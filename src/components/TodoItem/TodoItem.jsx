c210e045487cf31f9c1d758cf59b479f80d21111
VadimKorobec <_s_e_t__@ukr.net>
2023-06-23 15:44:02 +0300

    Update TodoItem.jsx

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,13 +1,14 @@
-export const TodoItem = ({
-  item: { title, completed },
-  handleCheckCompleted,
-}) => {
+export const TodoItem = ({ item, handleCheckCompleted }) => {
   return (
     <>
       <li style={{ listStyle: 'none', display: 'flex', gap: '20px' }}>
         <div>
-          <input type="checkbox" checked={completed} />
-          {title}
+          <input
+            type="checkbox"
+            checked={item.completed}
+            onChange={() => handleCheckCompleted(item.id)}
+          />
+          {item.title}
         </div>
 
         <button type="button">Delete Todo</button>
