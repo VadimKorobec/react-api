179c128676beddd58ccc94bf6a88a4e53eefe005
VadimKorobec <_s_e_t__@ukr.net>
2023-06-23 14:13:57 +0300

    Update Modal.jsx

--- src/components/Madal/Modal.jsx
+++ src/components/Madal/Modal.jsx
@@ -1,7 +1,14 @@
 export const Modal = ({ children }) => {
   return (
     <>
-      <div style={{ display: 'block', backdropFilter: 'blur(5px)' }}>
+      <div
+        style={{
+          display: 'block',
+          backdropFilter: 'blur(5px)',
+          width: '300px',
+          height: '300px',
+        }}
+      >
         <h5>Modal</h5>
         <button type="button">Click me</button>
         <div>{children}</div>
