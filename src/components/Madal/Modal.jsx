48edbb4175926f4e2f4f17c30d90ea68e2f74619
VadimKorobec <_s_e_t__@ukr.net>
2023-06-23 17:58:57 +0300

    Update Modal.jsx

--- src/components/Madal/Modal.jsx
+++ src/components/Madal/Modal.jsx
@@ -1,20 +1,21 @@
 export const Modal = ({ children, closeModal }) => {
   return (
     <>
-      <div
-        style={{
-          display: 'block',
-          backdropFilter: 'blur(5px)',
-          width: '300px',
-          height: '100px',
-          backgroundColor: 'gray',
-        }}
-      >
-        <h5>Modal</h5>
-        <button type="button" onClick={closeModal}>
-          Click me
-        </button>
-        <div>{children}</div>
+      <div className="modal-dialog" style={{ width: '400px' }}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal title</h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={closeModal}
+            ></button>
+          </div>
+
+          <div>{children}</div>
+        </div>
       </div>
     </>
   );
