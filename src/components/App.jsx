44bb7e1e8d8ea17d646d13b671034aebcaac8dae
VadimKorobec <_s_e_t__@ukr.net>
2023-06-23 14:49:08 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import { Modal } from './Madal/Modal';
 import { Counter } from './Counter/Counter';
+import { Header } from './Header/Header';
 
 export class App extends Component {
   state = {
-    isShowModal: true,
+    isShowModal: false,
   };
 
   showModal = () => {
     this.setState({ isShowModal: true });
   };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false });
+  };
+
   render() {
     return (
       <>
+        <Header showModal={this.showModal} />
         <Counter />
-        {this.state.isShowModal && <Modal>some</Modal>}
+        {this.state.isShowModal && (
+          <Modal closeModal={this.closeModal}>some</Modal>
+        )}
       </>
     );
   }
