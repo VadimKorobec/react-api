60953b97b8191fdd95116a18bca20372841b27c8
VadimKorobec <_s_e_t__@ukr.net>
2023-06-23 15:39:58 +0300

    Update TodoList.jsx

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,15 +1,31 @@
 import { Component } from 'react';
-import todoList from '../../todo.json';
+import todos from '../../todo.json';
 import { TodoItem } from 'components/TodoItem/TodoItem';
 
 export class TodoList extends Component {
+  state = {
+    todoList: todos,
+  };
+
+  handleCheckCompleted = id => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.map(item => item.id === id)
+        ? { ...todos, completed: !todos.completed }
+        : todos,
+    }));
+  };
+
   render() {
     return (
       <>
         <h1>My Todo List</h1>
         <ul>
-          {todoList.map(item => (
-            <TodoItem key={item.id} item={item} />
+          {this.state.todoList.map(item => (
+            <TodoItem
+              key={item.id}
+              item={item}
+              handleCheckCompleted={this.handleCheckCompleted}
+            />
           ))}
         </ul>
       </>
