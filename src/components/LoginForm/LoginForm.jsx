36452fbd26c15b5acb41abf867f632a19b020b12
VadimKorobec <_s_e_t__@ukr.net>
2023-06-24 18:12:38 +0300

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,21 +1,66 @@
 import { Component } from 'react';
 
 export class LoginForm extends Component {
-  state = {};
+  state = {
+    email: '',
+    password: '',
+    isChecked: false,
+    gender: 'male',
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleChack = ({ target: { checked } }) => {
+    this.setState({
+      isChecked: checked,
+    });
+  };
+
+  handleRadio = ({ target }) => {
+    this.setState({
+      gender: target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.createUser({
+      email: this.state.email,
+      password: this.state.password,
+      gender: this.state.gender,
+    });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState = {
+      email: '',
+      password: '',
+    };
+
+    this.props.closeModal();
+  };
   render() {
+    const { email, password, isChecked, gender } = this.state;
     return (
       <>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
+              name="email"
               type="email"
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
-              onChange={e => console.log(e.target.value)}
+              onChange={this.handleChange}
+              value={email}
             />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
@@ -26,9 +71,12 @@ export class LoginForm extends Component {
               Password
             </label>
             <input
+              name="password"
               type="password"
               className="form-control"
               id="exampleInputPassword1"
+              onChange={this.handleChange}
+              value={password}
             />
           </div>
           <div className="mb-3 form-check">
@@ -36,12 +84,46 @@ export class LoginForm extends Component {
               type="checkbox"
               className="form-check-input"
               id="exampleCheck1"
+              checked={isChecked}
+              onChange={this.handleChack}
             />
             <label className="form-check-label" htmlFor="exampleCheck1">
-              Check me out
+              I agree
             </label>
           </div>
-          <button type="submit" className="btn btn-primary">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault1"
+              checked={gender === 'male'}
+              onChange={this.handleRadio}
+              value="male"
+            />
+            <label className="form-check-label" htmlFor="flexRadioDefault1">
+              Male
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="flexRadioDefault2"
+              checked={gender === 'female'}
+              onChange={this.handleRadio}
+              value="female"
+            />
+            <label className="form-check-label" htmlFor="flexRadioDefault2">
+              Fimale
+            </label>
+          </div>
+          <button
+            disabled={!this.state.isChecked}
+            type="submit"
+            className="btn btn-primary"
+          >
             Submit
           </button>
         </form>
