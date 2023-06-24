3b1990e3a690fdd70324aa918f65c232c6335ba0
VadimKorobec <_s_e_t__@ukr.net>
2023-06-24 18:12:36 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -3,6 +3,32 @@ import { Component } from 'react';
 export class RegisterForm extends Component {
   state = {};
   render() {
-    return <></>;
+    return (
+      <>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+          />
+          <label className="form-check-label" for="flexRadioDefault1">
+            Default radio
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault2"
+            checked
+          />
+          <label className="form-check-label" for="flexRadioDefault2">
+            Default checked radio
+          </label>
+        </div>
+      </>
+    );
   }
 }
