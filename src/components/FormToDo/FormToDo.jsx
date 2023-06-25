fd5ebf156b2c79671e48997fc76da59915d8ac1b
VadimKorobec <_s_e_t__@ukr.net>
2023-06-25 19:36:42 +0300

    Create FormToDo.jsx

---
+++ src/components/FormToDo/FormToDo.jsx
@@ -0,0 +1,53 @@
+import { Component } from 'react';
+
+export class FormToDo extends Component {
+  state = {
+    todo: '',
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.addToDo(this.state.todo);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState = {
+      todo: '',
+    };
+  };
+
+  render() {
+    const { todo } = this.state;
+
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInput" className="form-label">
+              Create Todo
+            </label>
+            <input
+              name="todo"
+              type="text"
+              className="form-control"
+              id="exampleInput"
+              onChange={this.handleChange}
+              value={todo}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Add ToDo
+          </button>
+        </form>
+      </>
+    );
+  }
+}
