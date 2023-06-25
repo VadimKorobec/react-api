4df2b13e74897d215e8bb897040545ce166ffdb6
VadimKorobec <_s_e_t__@ukr.net>
2023-06-25 19:36:38 +0300

    Update Modal.jsx

--- src/components/Madal/Modal.jsx
+++ src/components/Madal/Modal.jsx
@@ -1,22 +1,43 @@
-export const Modal = ({ children, closeModal }) => {
-  return (
-    <>
-      <div className="modal-dialog" style={{ width: '400px' }}>
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Modal title</h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-              onClick={closeModal}
-            ></button>
-          </div>
+import { Component } from 'react';
 
-          <div>{children}</div>
+export class Modal extends Component {
+  state = {};
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handlePressESC);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handlePressESC);
+  }
+
+  handlePressESC = e => {
+    if (e.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    const { closeModal, children } = this.props;
+    return (
+      <>
+        <div className="modal-dialog" style={{ width: '400px' }}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                onClick={closeModal}
+              ></button>
+            </div>
+
+            <div>{children}</div>
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
