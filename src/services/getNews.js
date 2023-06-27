9db55ec680fff673554554212f74a8613984b379
VadimKorobec <_s_e_t__@ukr.net>
2023-06-27 17:29:44 +0300

    Update getNews.js

--- src/services/getNews.js
+++ src/services/getNews.js
@@ -1,6 +1,10 @@
-const BASE_URL = 'https://newsapi.org/v2/';
+const BASE_URL = 'https://newsapi.org/v2';
 const API_KEY = '8944f736087047f8879ae82d5af2a170';
 
 export const getNews = searchText => {
-  fetch(`${BASE_URL}/`);
+  return fetch(`${BASE_URL}/everything?q=${searchText}`, {
+    headers: {
+      'X-Api-Key': API_KEY,
+    },
+  });
 };
