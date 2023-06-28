803d73a9d9cc1f27f2b28a5cbfd4f4a1bb079544
VadimKorobec <_s_e_t__@ukr.net>
2023-06-28 18:47:52 +0300

    Update Modal.jsx

--- src/components/Madal/Modal.jsx
+++ src/components/Madal/Modal.jsx
@@ -1,43 +1,80 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
-export class Modal extends Component {
-  state = {};
+export const Modal = ({ children, closeModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handlePressESC);
+    return () => {
+      window.removeEventListener('keydown', handlePressESC);
+    };
+  }, []);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handlePressESC);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handlePressESC);
-  }
-
-  handlePressESC = e => {
+  const handlePressESC = e => {
     if (e.code === 'Escape') {
-      this.props.closeModal();
+      closeModal();
     }
   };
-
-  render() {
-    const { closeModal, children } = this.props;
-    return (
-      <>
-        <div className="modal-dialog" style={{ width: '400px' }}>
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-                onClick={closeModal}
-              ></button>
-            </div>
-
-            <div>{children}</div>
+  return (
+    <>
+      <div className="modal-dialog" style={{ width: '400px' }}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Modal title</h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={closeModal}
+            ></button>
           </div>
+
+          <div>{children}</div>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
+
+// import { Component } from 'react';
+
+// export class Modal extends Component {
+//   state = {};
+
+//   componentDidMount() {
+//    window .addEventListener('keydown', this.handlePressESC);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handlePressESC);
+//   }
+
+//   handlePressESC = e => {
+//     if (e.code === 'Escape') {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     const { closeModal, children } = this.props;
+//     return (
+//       <>
+//         <div className="modal-dialog" style={{ width: '400px' }}>
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h5 className="modal-title">Modal title</h5>
+//               <button
+//                 type="button"
+//                 className="btn-close"
+//                 data-bs-dismiss="modal"
+//                 aria-label="Close"
+//                 onClick={closeModal}
+//               ></button>
+//             </div>
+
+//             <div>{children}</div>
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+// }
