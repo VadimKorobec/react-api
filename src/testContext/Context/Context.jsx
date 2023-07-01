5762b21a041a21124257ba08c559cec0bc50d2e4
VadimKorobec <_s_e_t__@ukr.net>
2023-07-01 18:04:50 +0300

    Update Context.jsx

---
+++ src/testContext/Context/Context.jsx
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from 'react';
+
+const ContextAlert = React.createContext();
+
+export const useCustomContext = () => {
+  return useContext(ContextAlert);
+};
+
+export const Context = ({ children }) => {
+  const [toggleAlert, setToggleAlert] = useState(false);
+
+  return (
+    <ContextAlert.Provider
+      value={{ toggleValue: toggleAlert, toggle: setToggleAlert }}
+    >
+      {children}
+    </ContextAlert.Provider>
+  );
+};
