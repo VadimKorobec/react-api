8093f3ebcf6ba5f049b1ef349cb218f1b4e11142
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 18:11:46 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,33 @@
 import { Modal } from 'bootstrap';
 import { Header } from 'components/Header/Header';
 import { LoginForm } from 'components/LoginForm/LoginForm';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
+  const [isShowModal, setIsShowModal] = useState(false);
+
+  const showModal = () => {
+    setIsShowModal(true);
+  };
+
+  const closeModal = () => {
+    setIsShowModal(false);
+  };
+
+  const createUser = data => {
+    const newUser = {
+      ...data,
+      id: nanoid(),
+    };
+    console.log('newUser', newUser);
+  };
+
   return (
     <div className="container">
-      <Header />
+      <Header showModal={showModal} />
       <Outlet />
       {isShowModal && (
         <Modal closeModal={closeModal}>
