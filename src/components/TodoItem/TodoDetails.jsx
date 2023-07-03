da3cff35795fbf50f8a8ab97595f29447d159dfb
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 20:18:22 +0300

    Update TodoDetails.jsx

--- src/components/TodoItem/TodoDetails.jsx
+++ src/components/TodoItem/TodoDetails.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { TodoItem } from './TodoItem';
 
 export const TodoDetails = () => {
-  useParams();
-  return <>Todo Details</>;
+  const [todoList, setTodoList] = useState(null);
+  const params = useParams();
+
+  useEffect(() => {
+    const localTodo = localStorage.getItem('todo');
+    if (localTodo) setTodoList(JSON.parse(localTodo));
+  }, []);
+
+  return (
+    <>
+      {todoList?.map(
+        todo => todo.id === params.id && <TodoItem key={todo.id} todo={todo} />
+      )}
+    </>
+  );
 };
