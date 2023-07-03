19a7d9b5dbd39169b3770d77c60c04bce09266b9
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 18:11:33 +0300

    Update Context.jsx

--- src/testContext/Context/Context.jsx
+++ src/testContext/Context/Context.jsx
@@ -8,10 +8,16 @@ export const useCustomContext = () => {
 
 export const Context = ({ children }) => {
   const [toggleAlert, setToggleAlert] = useState(false);
+  const [news, setNews] = useState([]);
 
   return (
     <ContextAlert.Provider
-      value={{ toggleValue: toggleAlert, toggle: setToggleAlert }}
+      value={{
+        toggleValue: toggleAlert,
+        toggle: setToggleAlert,
+        news: news,
+        setNews: setNews,
+      }}
     >
       {children}
     </ContextAlert.Provider>
