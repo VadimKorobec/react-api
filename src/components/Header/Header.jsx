6c803a5819011fc8ce10e871e50b255db273e6c8
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 17:06:49 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 export const Header = ({ showModal }) => {
   return (
     <>
-      <nav>
-        <div>
-          <span>Navbar</span>
+      <nav className="navbar bg-dark mb-3">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 text-success ">Navbar</span>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+              <a className="nav-link" href="#">
+                Features
+              </a>
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+              <a className="nav-link disabled">Disabled</a>
+            </div>
+          </div>
           <button
+            className="btn btn-outline-seccess"
             type="button"
             style={{ backgroundColor: 'pink' }}
             onClick={showModal}
