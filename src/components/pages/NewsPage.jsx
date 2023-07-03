ed614a58be1ae5bc5df70ab10523e44f0a91059d
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 18:11:40 +0300

    Update NewsPage.jsx

--- src/components/pages/NewsPage.jsx
+++ src/components/pages/NewsPage.jsx
@@ -1,7 +1,19 @@
+import { ContentInfo } from 'components/ContentInfo/ContentInfo';
+import { Search } from 'components/Search/Search';
+import { useState } from 'react';
+
 export const NewsPage = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearch = searchText => {
+    setSearchText(searchText);
+  };
+
   return (
     <>
       <h1>News Page</h1>
+      <Search handleSearch={handleSearch} />
+      <ContentInfo searchText={searchText} />
     </>
   );
 };
