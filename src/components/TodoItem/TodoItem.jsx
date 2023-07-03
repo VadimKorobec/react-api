1dfa2f4002f50d6b76f5900bf30f820047d64bb9
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 19:26:55 +0300

    Update TodoItem.jsx

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,24 +1,32 @@
+import { Link } from 'react-router-dom';
+
 export const TodoItem = ({ todo, handleCheckCompleted, handleDelete }) => {
   return (
     <>
-      <li style={{ listStyle: 'none', display: 'flex', gap: '20px' }}>
-        <div>
-          <input
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => handleCheckCompleted(todo.id)}
-          />
-          {todo.title}
-        </div>
-
-        <button
-          type="button"
-          disabled={!todo.completed}
-          onClick={() => handleDelete(todo.id)}
-        >
-          Delete Todo
-        </button>
-      </li>
+      <Link to={todo.id}>
+        <li className="list-group-item">
+          <div className="row justify-content-between">
+            <div className="col-10">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => handleCheckCompleted(todo.id)}
+              />
+              {todo.title}
+            </div>
+            <div className="col">
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                disabled={!todo.completed}
+                onClick={() => handleDelete(todo.id)}
+              ></button>
+            </div>
+          </div>
+        </li>
+      </Link>
     </>
   );
 };
