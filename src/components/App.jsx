e7b9174b3cc9e4fa48d45138a37a3f1e3f412c37
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 16:33:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { NewsPage } from './pages/NewsPage';
+import { TodoPage } from './pages/TodoPage';
 
 export const App = () => {
   return (
     <Routes>
-      <Route path='/' element={} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/" element={<TodoPage />} />
     </Routes>
   );
 };
