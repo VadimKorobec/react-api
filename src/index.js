38380528dbf5a8fd965f0fbb4867d3847c876a55
VadimKorobec <_s_e_t__@ukr.net>
2023-07-03 18:11:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Context } from 'testContext/Context/Context';
 // import { App } from './testContext/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Context>
+        <App />
+      </Context>
     </BrowserRouter>
   </React.StrictMode>
 );
