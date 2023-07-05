9803c3c32032a77b7098a0fabdfc99560140b8c7
VadimKorobec <_s_e_t__@ukr.net>
2023-07-05 19:27:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import { NewsPage } from './pages/NewsPage';
-// import { TodoPage } from './pages/TodoPage';
 import Layout from './Layout/Layout';
 import HomePage from './pages/HomePage';
+
+// import { NewsPage } from './pages/NewsPage';
+// import { TodoPage } from './pages/TodoPage';
 // import { TodoDetails } from './TodoItem/TodoDetails';
-import { lazy } from 'react';
 
 const TodoDetails = lazy(() => import('./TodoItem/TodoDetails'));
 const TodoPage = lazy(() => import('./pages/TodoPage'));
 const NewsPage = lazy(() => import('./pages/NewsPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
 
 export const App = () => {
   return (
@@ -19,6 +21,7 @@ export const App = () => {
         <Route path="todo" element={<TodoPage />} />
         <Route path="todo/:id" element={<TodoDetails />} />
       </Route>
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 };
