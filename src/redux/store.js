72bd4c7e6bfece940ec6c087d229d990a9db7b90
VadimKorobec <_s_e_t__@ukr.net>
2023-07-07 18:50:39 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,4 @@
 import { createStore } from 'redux';
+import { reducer } from './reducer';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'increment':
-      return { ...state, total: action.payload };
-    case 'decrement':
-      return { ...state, total: action.payload };
-    default:
-      return state;
-  }
-};
-
-export const store = createStore(reducer, { total: 0 });
-
-console.log('store', store);
-
-store.dispatch({ type: 'increment', payload: 1 });
-
-console.log(store.getState());
+export const store = createStore(reducer);
