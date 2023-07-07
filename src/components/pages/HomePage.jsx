8cd047df40649fa15984a25ba2c413eb7f9beb74
VadimKorobec <_s_e_t__@ukr.net>
2023-07-07 18:50:34 +0300

    Update HomePage.jsx

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Counter } from 'components/Counter/Counter';
+import { Step } from 'components/Counter/Step';
 
 const HomePage = () => {
   return (
     <>
       <h1>Home Pages</h1>
+      <Step />
       <Counter />
     </>
   );
