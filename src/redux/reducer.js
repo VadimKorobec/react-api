55e71b691e7e80fe69169c899f4ca1ceb38e7389
VadimKorobec <_s_e_t__@ukr.net>
2023-07-07 20:03:52 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,32 +1,10 @@
 import { combineReducers } from 'redux';
 import { initialState } from './initialState';
-import { DECREMENT, INCREMENT, SETSTEP } from './types';
-
-const counterReducer = (state = initialState.counter, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        total: state.total + action.payload,
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        total: state.total - action.payload,
-      };
-    case SETSTEP:
-      return {
-        ...state,
-        step: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+import { CREATETODO, DECREMENT, INCREMENT, SETSTEP } from './types';
 
 const todoReducer = (state = initialState.todo, action) => {
   switch (action.type) {
-    case 'createTodo':
+    case CREATETODO:
       return {
         ...state,
         todo: [...state.todo, { ...action.payload }],
