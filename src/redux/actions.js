3aaedef1eb6a8fb1ec8499ce5b2b37bcfcd4967e
VadimKorobec <_s_e_t__@ukr.net>
2023-07-07 20:04:05 +0300

    Delete actions.js

--- src/redux/actions.js
+++
@@ -1,9 +0,0 @@
-import { DECREMENT, INCREMENT, SETSTEP } from './types';
-
-export const increment = step => ({ type: INCREMENT, payload: step });
-
-export const decrement = step => ({ type: DECREMENT, payload: step });
-
-export const setStep = value => ({ type: SETSTEP, payload: value });
-
-export const addTodo = () => {};
