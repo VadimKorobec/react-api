8dbd45dc57cc70292cfc91a3323f703657e9a8dd
VadimKorobec <_s_e_t__@ukr.net>
2023-07-07 16:45:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Context } from 'testContext/Context/Context';
 // import { App } from './testContext/App.jsx';
-import './redux/store';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/react-api">
-      <Context>
-        <App />
-      </Context>
+      <Provider store={store}>
+        <Context>
+          <App />
+        </Context>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
