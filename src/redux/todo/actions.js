ca32ea50386218f24fe0b87e9daa387928e9f3f2
VadimKorobec <_s_e_t__@ukr.net>
2023-07-08 16:37:38 +0300

    Update actions.js

--- src/redux/todo/actions.js
+++ src/redux/todo/actions.js
@@ -1,3 +1,10 @@
-import { CREATETODO } from './types';
+import { createAction } from '@reduxjs/toolkit';
+// import { CREATETODO } from './types';
 
-export const createTodo = todo => ({ type: CREATETODO, payload: todo });
+// export const createTodo = todo => ({ type: CREATETODO, payload: todo });
+
+export const createTodo = createAction('CREATETODO', () => {
+    return {
+        payload:
+    }
+});
