a84fbf07a786a9dba669363e2a258855ffc74d68
VadimKorobec <_s_e_t__@ukr.net>
2023-07-08 16:37:44 +0300

    Update actions.js

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,7 +1,12 @@
-import { DECREMENT, INCREMENT, SETSTEP } from './types';
+import { createAction } from '@reduxjs/toolkit';
+// import { DECREMENT, INCREMENT, SETSTEP } from './types';
 
-export const increment = step => ({ type: INCREMENT, payload: step });
+// export const increment = step => ({ type: INCREMENT, payload: step });
+// export const decrement = step => ({ type: DECREMENT, payload: step });
+// export const setStep = value => ({ type: SETSTEP, payload: value });
 
-export const decrement = step => ({ type: DECREMENT, payload: step });
+export const increment = createAction('INCREMENT');
 
-export const setStep = value => ({ type: SETSTEP, payload: value });
+export const decrement = createAction('DECREMENT');
+
+export const setStep = createAction('SETSTEP');
