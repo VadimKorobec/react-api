f58808288a7da3af6e80f4693a0aa5ddcd9cdbb3
VadimKorobec <_s_e_t__@ukr.net>
2023-07-08 15:36:44 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
 import { reducer } from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
 
-export const store = createStore(reducer);
+export const store = configureStore({ reducer });
