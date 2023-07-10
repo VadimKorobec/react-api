97e9f2e26ed089134db02fa0708db68a400e20a4
VadimKorobec <_s_e_t__@ukr.net>
2023-07-10 16:22:04 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
-import { counterReducer } from './counter/counterReducer';
+// import { counterReducer } from './counter/counterReducer';
 import { todoReducer } from './todo/todoReducer';
+import { counterReducer } from './counter/counterSlice';
 
 export const reducer = combineReducers({
   counter: counterReducer,
