6c9eb780ecd07497e86fa6df78167cd4a5d69efc
VadimKorobec <_s_e_t__@ukr.net>
2023-07-10 16:22:15 +0300

    Update Step.jsx

--- src/components/Counter/Step.jsx
+++ src/components/Counter/Step.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { setStep } from 'redux/counter/actions';
+import { setStep } from '../../redux/counter/counterSlice';
 
 export const Step = () => {
   const { step } = useSelector(state => state.counter);
