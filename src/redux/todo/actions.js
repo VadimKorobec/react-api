af7ba1fec2438f39d3a1487b0b8db2a94c961b46
VadimKorobec <_s_e_t__@ukr.net>
2023-07-10 15:05:13 +0300

    Update actions.js

--- src/redux/todo/actions.js
+++ src/redux/todo/actions.js
@@ -1,10 +1,14 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, nanoid } from '@reduxjs/toolkit';
 // import { CREATETODO } from './types';
 
 // export const createTodo = todo => ({ type: CREATETODO, payload: todo });
 
-export const createTodo = createAction('CREATETODO', () => {
-    return {
-        payload:
-    }
+export const createTodo = createAction('CREATETODO', value => {
+  return {
+    payload: {
+      id: nanoid(),
+      title: value,
+      completed: false,
+    },
+  };
 });
