094780716b91bd05b4ea98220c8565d6ccba9b0b
VadimKorobec <_s_e_t__@ukr.net>
2023-07-13 16:25:43 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,23 +9,23 @@ const persistConfig = {
   blacklist: ['counter'],
 };
 
-const customMidle = state => {
-  return next => {
-    return action => {
-      if (typeof action === 'function') {
-        console.log('state', state);
-        return next(action());
-      }
-      return next(action);
-    };
-  };
-};
+// const customMidle = state => {
+//   return next => {
+//     return action => {
+//       if (typeof action === 'function') {
+//         action(state.dispatch);
+//         return;
+//       }
+//       return next(action);
+//     };
+//   };
+// };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: [customMidle],
+  // middleware: [customMidle],
 });
 
 export const persistor = persistStore(store);
