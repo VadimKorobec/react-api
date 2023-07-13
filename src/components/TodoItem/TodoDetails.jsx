37c8723829694c480fb789c7cca60dbbdfe9854e
VadimKorobec <_s_e_t__@ukr.net>
2023-07-13 14:46:54 +0300

    Update TodoDetails.jsx

--- src/components/TodoItem/TodoDetails.jsx
+++ src/components/TodoItem/TodoDetails.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense } from 'react';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import { TodoItem } from './TodoItem';
 import { useSelector } from 'react-redux';
