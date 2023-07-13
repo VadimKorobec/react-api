8250412acb1a47b5ab451d8c39b771d695e58831
VadimKorobec <_s_e_t__@ukr.net>
2023-07-13 17:41:35 +0300

    Update newsReducer.js

--- src/redux/news/newsReducer.js
+++ src/redux/news/newsReducer.js
@@ -1,5 +1,47 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getTopNews } from 'services/getNews';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { getNewsSearchThunk, getNewsThunk } from './thunk';
+import { initialState } from './initialState';
+
+const customArr = [getNewsThunk, getNewsSearchThunk];
+
+const fn = status => {
+  return customArr.map(item => item[status]);
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.news = action.payload;
+  state.error = '';
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const newsSlice = createSlice({
+  name: 'news',
+  initialState,
+  extraReducers: builder => {
+    builder
+      // .addCase(getNewsThunk.pending, handlePending)
+      .addCase(getNewsThunk.fulfilled, handleFulfilled)
+      .addCase(getNewsThunk.rejected, handleRejected)
+      // .addCase(getNewsSearchThunk.pending, handlePending)
+      .addCase(getNewsSearchThunk.fulfilled, handleFulfilled)
+      .addCase(getNewsSearchThunk.rejected, handleRejected)
+      .addMatcher(
+        isAnyOf([getNewsThunk.pending, getNewsSearchThunk.pending]),
+        handlePending
+      );
+  },
+});
+
+export const newsReducer = newsSlice.reducer;
 
 // export const getNewsThunk = () => {
 //   return async dispatch => {
@@ -13,34 +55,50 @@ import { getTopNews } from 'services/getNews';
 //   };
 // };
 
-export const getNewsThunk = createAsyncThunk('news/getTopNews', async () => {
-  return await getTopNews();
-});
-
-const initialState = {
-  news: [],
-  isLoading: false,
-  error: '',
-};
-
-const newsSlice = createSlice({
-  name: 'news',
-  initialState,
-  // reducers: {
-  //   fetching: state => {
-  //     state.isLoading = true;
-  //   },
-  //   fetchSuccess: (state, action) => {
-  //     state.isLoading = false;
-  //     state.news = action.payload;
-  //     state.error = '';
-  //   },
-  //   fetchError: (state, { payload }) => {
-  //     state.isLoading = false;
-  //     state.error = payload;
-  //   },
-  // },
-  extraReducers: {},
-});
-
-export const newsReducer = newsSlice.reducer;
+// const newsSlice = createSlice({
+//   name: 'news',
+//   initialState,
+// reducers: {
+//   fetching: state => {
+//     state.isLoading = true;
+//   },
+//   fetchSuccess: (state, action) => {
+//     state.isLoading = false;
+//     state.news = action.payload;
+//     state.error = '';
+//   },
+//   fetchError: (state, { payload }) => {
+//     state.isLoading = false;
+//     state.error = payload;
+//   },
+// },
+// extraReducers: {
+//   [getNewsThunk.pending](state) {
+//     state.isLoading = true;
+//   },
+//   [getNewsThunk.fulfilled](state, action) {
+//     state.isLoading = false;
+//     state.news = action.payload;
+//     state.error = '';
+//   },
+//   [getNewsThunk.rejected](state, action) {
+//     state.isLoading = false;
+//     state.error = action.payload;
+//   },
+// },
+// extraReducers: builder => {
+//   builder
+//     .addCase(getNewsThunk.pending, state => {
+//       state.isLoading = true;
+//     })
+//     .addCase(getNewsThunk.fulfilled, (state, action) => {
+//       state.isLoading = false;
+//       state.news = action.payload;
+//       state.error = '';
+//     })
+//     .addCase(getNewsThunk.rejected, (state, action) => {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     });
+// },
+// });
