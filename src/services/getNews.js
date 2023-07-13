44fbbab6f249fd0767d0998bf76305e39e06bc04
VadimKorobec <_s_e_t__@ukr.net>
2023-07-13 17:41:32 +0300

    Update getNews.js

--- src/services/getNews.js
+++ src/services/getNews.js
@@ -1,12 +1,14 @@
 const BASE_URL = 'https://newsapi.org/v2';
 const API_KEY = '8944f736087047f8879ae82d5af2a170';
 
-export const getNews = searchText => {
-  return fetch(`${BASE_URL}/everything?q=${searchText}`, {
+export const getNews = async searchText => {
+  const data = await fetch(`${BASE_URL}/everything?q=${searchText}`, {
     headers: {
       'X-Api-Key': API_KEY,
     },
   });
+
+  return await data.json();
 };
 
 export const getTopNews = async () => {
