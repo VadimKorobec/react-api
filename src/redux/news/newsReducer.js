7cd9a0c02b23d8814d49b0c31597330621e14fd4
VadimKorobec <_s_e_t__@ukr.net>
2023-07-13 16:25:47 +0300

    Update newsReducer.js

--- src/redux/news/newsReducer.js
+++ src/redux/news/newsReducer.js
@@ -1,3 +1,46 @@
-export const getNewsThunk = () => {
-  return () => ({ type: 'thunk', payload: 100 });
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getTopNews } from 'services/getNews';
+
+// export const getNewsThunk = () => {
+//   return async dispatch => {
+//     try {
+//       dispatch(newsSlice.actions.fetching());
+//       const data = await getTopNews();
+//       dispatch(newsSlice.actions.fetchSuccess(data));
+//     } catch (error) {
+//       dispatch(newsSlice.actions.fetchError(error));
+//     }
+//   };
+// };
+
+export const getNewsThunk = createAsyncThunk('news/getTopNews', async () => {
+  return await getTopNews();
+});
+
+const initialState = {
+  news: [],
+  isLoading: false,
+  error: '',
 };
+
+const newsSlice = createSlice({
+  name: 'news',
+  initialState,
+  // reducers: {
+  //   fetching: state => {
+  //     state.isLoading = true;
+  //   },
+  //   fetchSuccess: (state, action) => {
+  //     state.isLoading = false;
+  //     state.news = action.payload;
+  //     state.error = '';
+  //   },
+  //   fetchError: (state, { payload }) => {
+  //     state.isLoading = false;
+  //     state.error = payload;
+  //   },
+  // },
+  extraReducers: {},
+});
+
+export const newsReducer = newsSlice.reducer;
