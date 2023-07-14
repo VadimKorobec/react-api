70798dad6cdca8d8bbbf571aacbc08f5ca47f3d2
VadimKorobec <_s_e_t__@ukr.net>
2023-07-14 18:24:35 +0300

    Update selectors.js

--- src/redux/product/selectors.js
+++ src/redux/product/selectors.js
@@ -1,6 +1,3 @@
 export const productsSelector = state => {
-   
-    
-    return [...state.products.products].sort((a, b) => a.price - b.price),
-  
+  return [...state.products.products].sort((a, b) => a.price - b.price);
 };
