ec72f06beac790386434e7c2c992b780d19319dc
VadimKorobec <_s_e_t__@ukr.net>
2023-07-14 15:45:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,7 @@ const TodoDetails = lazy(() => import('./TodoItem/TodoDetails'));
 const TodoPage = lazy(() => import('./pages/TodoPage'));
 const NewsPage = lazy(() => import('./pages/NewsPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
+const ProductsPage = lazy(() => import('./pages/ProductsPage'));
 
 export const App = () => {
   return (
@@ -19,6 +20,7 @@ export const App = () => {
         <Route index element={<HomePage />} />
         <Route path="news" element={<NewsPage />} />
         <Route path="todo" element={<TodoPage />} />
+        <Route path="products" element={<ProductsPage />} />
         <Route path="todo/:id" element={<TodoDetails />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
