f7db4cdf09774d9d159d33f2bd80e0d708276c08
VadimKorobec <_s_e_t__@ukr.net>
2023-07-14 18:02:32 +0300

    Update slice.js

--- src/redux/product/slice.js
+++ src/redux/product/slice.js
@@ -1,15 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
-import { getProductsThunk } from './thunk';
+import {
+  createProductsThunk,
+  deleteProductsThunk,
+  getProductsThunk,
+} from './thunk';
 
 const handlePending = state => {
   state.isLoading = true;
 };
 
-const handleFulfilled = (state, action) => {
+const handleFulfilled = state => {
   state.isLoading = false;
-  state.products = action.payload;
   state.error = '';
+};
+
+const handleFulfilledGet = (state, action) => {
+  state.products = action.payload;
+  //   handleFulfilled(state);
+};
+
+const handleFulfilledCreate = (state, action) => {
+  state.products.push(action.payload);
+  //   handleFulfilled(state);
+};
+
+const handleFulfilledDelete = (state, action) => {
+  state.products = state.products.filter(item => item.id !== action.payload.id);
+  // handleFulfilled(state)
 };
 
 const handleRejected = (state, action) => {
@@ -17,13 +35,45 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
-export const productSlice = createSlice({
+export const productsSlice = createSlice({
   name: 'products',
   initialState,
   extraReducers: builder => {
     builder
-      .addCase(getProductsThunk.pending, handlePending)
-      .addCase(getProductsThunk.fulfilled, handleFulfilled)
-      .addCase(getProductsThunk.rejected, handleRejected);
+      //   .addCase(getProductsThunk.pending, handlePending)
+      .addCase(getProductsThunk.fulfilled, handleFulfilledGet)
+      //   .addCase(getProductsThunk.rejected, handleRejected)
+      //   .addCase(createProductsThunk.pending, handlePending)
+      .addCase(createProductsThunk.fulfilled, handleFulfilledCreate)
+      //   .addCase(createProductsThunk.rejected, handleRejected)
+      //   .addCase(deleteProductsThunk.pending, handlePending)
+      .addCase(deleteProductsThunk.fulfilled, handleFulfilledDelete)
+      //   .addCase(deleteProductsThunk.rejected, handleRejected)
+      .addMatcher(
+        isAnyOf(
+          getProductsThunk.pending,
+          createProductsThunk.pending,
+          createProductsThunk.rejected
+        ),
+        handlePending
+      )
+      .addMatcher(
+        isAnyOf(
+          getProductsThunk.rejected,
+          createProductsThunk.rejected,
+          deleteProductsThunk.rejected
+        ),
+        handleRejected
+      )
+      .addMatcher(
+        isAnyOf(
+          getProductsThunk.fulfilled,
+          createProductsThunk.fulfilled,
+          deleteProductsThunk.fulfilled
+        ),
+        handleFulfilled
+      );
   },
 });
+
+export const productsReducer = productsSlice.reducer;
