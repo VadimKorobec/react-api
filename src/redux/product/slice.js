6a3815ced9d19bb2f3652d3e79d018254f15fa6e
VadimKorobec <_s_e_t__@ukr.net>
2023-07-14 15:53:53 +0300

    Update slice.js

--- src/redux/product/slice.js
+++ src/redux/product/slice.js
@@ -1,10 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
+import { getProductsThunk } from './thunk';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.products = action.payload;
+  state.error = '';
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 export const productSlice = createSlice({
   name: 'products',
   initialState,
   extraReducers: builder => {
-    builder.addCase();
+    builder
+      .addCase(getProductsThunk.pending, handlePending)
+      .addCase(getProductsThunk.fulfilled, handleFulfilled)
+      .addCase(getProductsThunk.rejected, handleRejected);
   },
 });
