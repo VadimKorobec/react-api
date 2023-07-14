e179e07b75d8fb6b42dd28ac719e2a0d04459153
VadimKorobec <_s_e_t__@ukr.net>
2023-07-14 18:02:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import HomePage from './pages/HomePage';
+import { ProductsPage } from './pages/ProductsPage';
 
 // import { NewsPage } from './pages/NewsPage';
 // import { TodoPage } from './pages/TodoPage';
@@ -11,7 +12,6 @@ const TodoDetails = lazy(() => import('./TodoItem/TodoDetails'));
 const TodoPage = lazy(() => import('./pages/TodoPage'));
 const NewsPage = lazy(() => import('./pages/NewsPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage'));
-const ProductsPage = lazy(() => import('./pages/ProductsPage'));
 
 export const App = () => {
   return (
