4026693cea73e7870d5afc8722f29d810211abee
VadimKorobec <_s_e_t__@ukr.net>
2023-07-15 16:21:16 +0300

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,10 +4,12 @@ import { todoReducer } from './todo/todoReducer';
 import { counterReducer } from './counter/counterSlice';
 import { newsReducer } from './news/newsReducer';
 import { productsReducer } from './product/slice';
+import { productsApi } from './product/productsApi';
 
 export const reducer = combineReducers({
   counter: counterReducer,
   todo: todoReducer,
   news: newsReducer,
   products: productsReducer,
+  [productsApi.reducerPath]: productsApi.reducer,
 });
