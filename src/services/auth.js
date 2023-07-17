eb9e0d6c752296943e47bd17023b07a8e7ad976f
VadimKorobec <_s_e_t__@ukr.net>
2023-07-17 18:03:50 +0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,10 +4,25 @@ const instance = axios.create({
   baseURL: 'https://api.escuelajs.co/api/v1',
 });
 
+const setToken = token => {
+  instance.defaults.headers.common['Authorization'] = token;
+};
+
+export const dellToken = () => {
+  delete instance.defaults.headers.common['Authorization'];
+};
+
 export const singUp = async body => {
   return await instance.post('/users', body);
 };
 
 export const login = async body => {
-  return await instance.get('//auth/login', body);
+  const { data } = await instance.post('/auth/login', body);
+  setToken(`Bearer ${data.access_token}`);
+  return data;
+};
+
+export const getProfile = async () => {
+  const { data } = instance.get('/auth/profile');
+  return data;
 };
