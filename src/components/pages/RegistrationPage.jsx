463b6ee54a93e3dab4728d6b3b600fdbac90476a
VadimKorobec <_s_e_t__@ukr.net>
2023-07-17 15:04:33 +0300

    Create RegistrationPage.jsx

---
+++ src/components/pages/RegistrationPage.jsx
@@ -0,0 +1,58 @@
+export const RegistrationPage = () => {
+  return (
+    <>
+      <div
+        className="card position-absolute top-50 start-50 translate-middle p-2  "
+        style={{ minWidth: '350px' }}
+      >
+        <h1 className="text-center">SignUp</h1>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName1" className="form-label">
+              Name
+            </label>
+            <input
+              name="name"
+              type="text"
+              className="form-control"
+              id="exampleInputName1"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              name="email"
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button
+            //   disabled={!this.state.isChecked}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
