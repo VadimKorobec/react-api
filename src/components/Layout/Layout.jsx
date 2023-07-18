32406c38f9849ef5f491c1b145893d4995df3704
VadimKorobec <_s_e_t__@ukr.net>
2023-07-18 17:39:30 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import { Header } from 'components/Header/Header';
 import { LoginForm } from 'components/LoginForm/LoginForm';
 import { nanoid } from 'nanoid';
 import { Suspense, useState } from 'react';
-import { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
@@ -37,7 +36,6 @@ const Layout = () => {
             <LoginForm createUser={createUser} closeModal={closeModal} />
           </Modal>
         )}
-        <Toaster position="top-right" reverseOrder={false} />
       </div>
     </>
   );
