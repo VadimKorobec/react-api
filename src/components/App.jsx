0606156d47f7e40196674cbec402933246407696
VadimKorobec <_s_e_t__@ukr.net>
2023-07-18 18:21:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,9 @@ import HomePage from './pages/HomePage';
 import { ProductsPage } from './pages/ProductsPage';
 import LoginPage from './pages/LoginPage';
 import { RegistrationPage } from './pages/RegistrationPage';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
+import { PrivateRoute } from './PrivateRoute';
 
 // import { NewsPage } from './pages/NewsPage';
 // import { TodoPage } from './pages/TodoPage';
@@ -17,21 +18,26 @@ const TodoPage = lazy(() => import('./pages/TodoPage'));
 const NewsPage = lazy(() => import('./pages/NewsPage'));
 
 export const App = () => {
-  const isAuth = useSelector(state => state.auth.access_token);
+  // const isAuth = useSelector(state => state.auth.access_token);
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="news" element={<NewsPage />} />
-          {isAuth && (
-            <>
-              {' '}
-              <Route path="todo" element={<TodoPage />} />
-              <Route path="products" element={<ProductsPage />} />
-              <Route path="todo/:id" element={<TodoDetails />} />
-            </>
-          )}
+
+          <>
+            <Route
+              path="todo"
+              element={
+                <PrivateRoute>
+                  <TodoPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="products" element={<ProductsPage />} />
+            <Route path="todo/:id" element={<TodoDetails />} />
+          </>
         </Route>
         <Route path="/login" element={<LoginPage />} />
         <Route
