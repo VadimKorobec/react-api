c49cf96de2bdb43c613f6d5cbe9e6b64a8634dd1
VadimKorobec <_s_e_t__@ukr.net>
2023-07-19 15:37:05 +0300

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,5 +3,6 @@ import { Navigate } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
   const isAuth = useSelector(state => state.auth.access_token);
+
   return isAuth ? children : <Navigate to="/login" />;
 };
