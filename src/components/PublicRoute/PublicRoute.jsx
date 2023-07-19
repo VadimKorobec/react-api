9bad34eb6ffb5b3827111e398935df13230a9bb3
VadimKorobec <_s_e_t__@ukr.net>
2023-07-19 16:11:04 +0300

    Update PublicRoute.jsx

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,8 +1,10 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const PublicRoute = ({ children }) => {
   const isAuth = useSelector(state => state.auth.access_token);
+  const location = useLocation();
+  console.log('location', location);
 
   return !isAuth ? children : <Navigate to="/" />;
 };
