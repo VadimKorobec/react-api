96b8960e6cf84aa314aad4d00fdb5afaeb774166
VadimKorobec <_s_e_t__@ukr.net>
2023-07-19 16:11:02 +0300

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
   const isAuth = useSelector(state => state.auth.access_token);
+  const location = useLocation();
 
-  return isAuth ? children : <Navigate to="/login" />;
+  return isAuth ? children : <Navigate to="/login" state={location} />;
 };
